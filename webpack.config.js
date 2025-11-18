e73c21613bde4b82db71b8bf9629f0ad7f2e27b6
Stanislav <stalevs@gmail.com>
2025-11-18 23:24:32 +0100

    add webpack config and missing plugins and dependencies

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+/* eslint-env node */
+
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import Dotenv from "dotenv-webpack";
+
+module.exports = {
+  entry: "./src/main.tsx",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+    new Dotenv(),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
