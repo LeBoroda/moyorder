019d68eabe9cb484871656a694c6377becb10811
Stanislav <stalevs@gmail.com>
2025-11-18 22:19:33 +0100

    add and run prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -8,18 +8,33 @@ import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-	{
-		files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-		plugins: { js, react: pluginReact },
-		extends: ["js/recommended", pluginReact.configs.flat.recommended],
-		languageOptions: { globals: globals.browser },
-		settings: { react: { version: "detect" } },
-	},
-	tseslint.configs.recommended,
-	{ files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-	{ files: ["**/*.md"], plugins: { markdown }, language: "markdown/commonmark", extends: ["markdown/recommended"] },
-	{ files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
-	{
-		ignores: ["package.json", "package-lock.json"]
-	}
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js, react: pluginReact },
+    extends: ["js/recommended", pluginReact.configs.flat.recommended],
+    languageOptions: { globals: globals.browser },
+    settings: { react: { version: "detect" } },
+  },
+  tseslint.configs.recommended,
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/commonmark",
+    extends: ["markdown/recommended"],
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+  },
+  {
+    ignores: ["package.json", "package-lock.json"],
+  },
 ]);
