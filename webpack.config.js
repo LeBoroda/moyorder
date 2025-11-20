4093f90ff752570db1b92d076ca751f76ff110ab
Stanislav <stalevs@gmail.com>
2025-11-21 00:04:53 +0100

    add proxy settings for dev moysklad

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import path from "path";
 import Dotenv from "dotenv-webpack";
+import webpack from "webpack";
+import { config } from "dotenv";
+
+// Load .env file to get values for DefinePlugin
+config();
 
 export default {
   entry: "./src/main.tsx",
@@ -28,7 +33,31 @@ export default {
     new HtmlWebpackPlugin({
       template: "index.html",
     }),
-    new Dotenv(),
+    new Dotenv({
+      systemvars: true,
+    }),
+    // Explicitly define environment variables for dynamic access
+    new webpack.DefinePlugin({
+      "process.env.MOYSKLAD_USERNAME": JSON.stringify(
+        process.env.MOYSKLAD_USERNAME,
+      ),
+      "process.env.MOYSKLAD_PASSWORD": JSON.stringify(
+        process.env.MOYSKLAD_PASSWORD,
+      ),
+      "process.env.MOYSKLAD_TOKEN": JSON.stringify(process.env.MOYSKLAD_TOKEN),
+      "process.env.ORDER_NOTIFICATION_EMAIL": JSON.stringify(
+        process.env.ORDER_NOTIFICATION_EMAIL,
+      ),
+      "process.env.EMAILJS_PUBLIC_KEY": JSON.stringify(
+        process.env.EMAILJS_PUBLIC_KEY,
+      ),
+      "process.env.EMAILJS_SERVICE_ID": JSON.stringify(
+        process.env.EMAILJS_SERVICE_ID,
+      ),
+      "process.env.EMAILJS_TEMPLATE_ID": JSON.stringify(
+        process.env.EMAILJS_TEMPLATE_ID,
+      ),
+    }),
   ],
   devServer: {
     static: {
@@ -36,5 +65,16 @@ export default {
     },
     compress: true,
     port: 9000,
+    proxy: [
+      {
+        context: ["/api/moysklad"],
+        target: "https://api.moysklad.ru/api/remap/1.2",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api/moysklad": "",
+        },
+        secure: true,
+      },
+    ],
   },
 };
