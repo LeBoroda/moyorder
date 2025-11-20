ab6329d4a88e09bed0ff9f3885e0480ed7885aa9
Stanislav <stalevs@gmail.com>
2025-11-20 22:47:46 +0100

    fix typo

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./main.css";
-import "./observers/appLogger";
+import "./utils/appLoggers";
 import React from "react";
 import App from "./App";
 
