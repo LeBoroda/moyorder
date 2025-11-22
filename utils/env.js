df11013f838e134b7bb45c0e193e306b8caf7b4f
LeBoroda <LeBoroda@users.noreply.github.com>
2025-11-22 07:19:26 +0000

    deploy: b7ebcc0bd53afe175dab112181386eac0219368a

--- utils/env.js
+++ utils/env.js
@@ -9,6 +9,8 @@ function readEnv(key) {
             return process.env.MOYSKLAD_PASSWORD;
         case "MOYSKLAD_TOKEN":
             return process.env.MOYSKLAD_TOKEN;
+        case "MOYSKLAD_CORS_PROXY":
+            return process.env.MOYSKLAD_CORS_PROXY;
         case "ORDER_NOTIFICATION_EMAIL":
             return process.env.ORDER_NOTIFICATION_EMAIL;
         case "EMAILJS_PUBLIC_KEY":
